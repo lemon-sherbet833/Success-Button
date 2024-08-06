ab7a13bac8a3d727a65698b9f20320ee881a2d1a
Kevin Lu <kevin12358@protonmail.com>
2024-08-06 16:30:26 -0700

    Made program buildable into executable

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
 ## Success Button
 
 Plays a cute little sound of encouragement to provide you a boost of motivation. I mainly created this application for personal use.
+
+
+## Build Instructions:
+Install pyinstaller:
+```
+pip install pyinstaller
+```
+
+### Then build the exe file:
+cd to the directory of this project. Then run the following command.
+
+Windows:
+```
+pyinstaller --onefile --windowed --add-data "success_sound.mp3;." --add-data "window_icon.png;." --icon=file_icon.ico success_button.py
+```
+
+Mac/Linux:
+```
+pyinstaller --onefile --windowed --add-data "success_sound.mp3:." --add-data "window_icon.png:." --icon=file_icon.ico success_button.py
+```
+
+And the desired executable will be in the dist folder.
--- success_button.py
+++ success_button.py
@@ -2,13 +2,25 @@ import tkinter
 from playsound import playsound
 import threading
 import pygame
+import os
+import sys
 
 maxChannels = 3
 targetChannel = 0
 
+# File handing function that universally works for local testing, and
+# pyinstaller packaged files
+def getFile(relative_path):
+    try:
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
+
 pygame.mixer.init()
 pygame.mixer.set_num_channels(maxChannels)
-success_sound = pygame.mixer.Sound("success sound.mp3")
+success_sound = pygame.mixer.Sound(getFile("success_sound.mp3"))
 
 def playSuccess(channelNum):
     pygame.mixer.Channel(channelNum).play(success_sound)
@@ -25,7 +37,7 @@ def startPSThread():
 
 rootWindow = tkinter.Tk()
 rootWindow.title("Dopamine")
-rootWindow.iconphoto(True, tkinter.PhotoImage(file="window icon.png"))
+rootWindow.iconphoto(True, tkinter.PhotoImage(file=getFile("window_icon.png")))
 rootWindow.geometry("250x50")
 
 button = tkinter.Button(rootWindow, text="Success!", command=startPSThread, height=2, width=15)
